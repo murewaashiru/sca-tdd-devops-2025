316fda3243c4692afbc3673a89deecb526a2ef19
Omomurewa George-Ashiru <rachelleashiru@gmail.com>
2025-08-18 19:58:03 +0100

    fix:env file

--- mongodb-express-rest-api/app/src/config.js
+++ mongodb-express-rest-api/app/src/config.js
@@ -1,6 +1,4 @@
 import dotenv from "dotenv";
-
 dotenv.config();
 
-console.log("Environment Variables Loaded: ", process.env.BACKEND_URL);
 export const baseUrl = process.env.BACKEND_URL || "http://localhost:5050";
